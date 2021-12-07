d26f0fd3d5bdadeaeee5cd7820f989d203feb070
冠龍 李 <nionismine@yahoo.com.tw>
2021-12-07 15:45:05 +0800

    Add project files.

---
+++ RedisClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+var db = redis.GetDatabase();
+
+Console.Write("請輸入欲查詢之key：");
+var input = Console.ReadLine();
+
+var value = await db.StringGetAsync(input);
+var result = string.IsNullOrEmpty(value.ToString()) ? "(X)該鍵值目前無對應資料" : value.ToString();
+
+Console.WriteLine("其值為：" + result + "\n");
+
+Console.WriteLine("請輸入欲訂閱之主題");
+var topic = Console.ReadLine();
+
+var sub = redis.GetSubscriber();
+await sub.SubscribeAsync(topic, (_, message) =>
+{
+    Console.WriteLine($"來自主題{topic}推播之訊息：{(string)message}");
+});
+
+Console.WriteLine("已成功訂閱！");
+Console.WriteLine("收播中...\n");
+
+Console.ReadKey();
---
+++ RedisServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using StackExchange.Redis;
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+var db = redis.GetDatabase();
+
+Console.Write("請輸入欲設定之key：");
+var input = Console.ReadLine();
+
+Console.Write(input + "值設定為：");
+var value = Console.ReadLine();
+
+await db.StringSetAsync(input, value);
+Console.WriteLine("設定完成！\n");
+
+Console.WriteLine("請點擊任一按鍵接續Pub/Sub範例--\n");
+Console.ReadKey();
+
+Console.Write("\n請輸入欲推播之主題：");
+var topic = Console.ReadLine();
+
+
+string? content;
+var sub = redis.GetSubscriber();
+while (true)
+{
+    Console.Write("請輸入推播內容：");
+    content = Console.ReadLine();
+
+    await sub.PublishAsync(topic, content);
+}
